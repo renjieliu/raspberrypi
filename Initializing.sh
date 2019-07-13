d44bef6ec50b66d7bffe2ad08921f37769898c99
renjieliu <anlrj@qq.com>
2019-07-13 13:53:36 -0400

    Update Initializing.sh

--- Initializing.sh
+++ Initializing.sh
@@ -1,7 +1,7 @@
 passwd;
 sudo passwd;
 sudo apt-get update; 
-sudo apt-get install -y locate mariadb-server tightvncserver octave; 
+sudo apt-get install -y locate mariadb-server tightvncserver cmatrix octave; 
 sudo updatedb;
 sudo echo 'interface wlan0
 static ip_address=192.168.1.205/24
@@ -11,3 +11,7 @@ static domain_name_servers=192.168.1.1 8.8.8.8 fd51:42f8:caae:d92e::1'  >> /etc/
 echo alias python='python3.7' >> ~/.bashrc;
 mkdir ~/Share; sudo chmod 777 -R ~/Share;
 sudo mkdir /mnt/sandisk; sudo chmod 777 -R /mnt/sandisk;
+sudo curl -sSL https://get.docker.com | sh;
+
+
+sudo raspi-config;
