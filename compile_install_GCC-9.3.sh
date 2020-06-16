608e786bba7f583cf66ec0aebfea261ee17ffc00
renjieliu <anlrj@qq.com>
2020-06-16 17:06:19 -0400

    Add files via upload

---
+++ compile_install_GCC-9.3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 
+# compile and build GCC 9.3 on Rpi, ARMv7 32 bit OS
+# run on the latest ubuntu docker image
+# the image does not have any existing C / C++ compiler, we need to install them first, in order to compile and build GCC-9
+
+apt-get update;
+apt-get install -y git wget gcc g++ make;
+mkdir gcc;
+cd gcc;
+export GCC_VERSION=9.3.0; #The version can be changed here, as needed
+wget https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.gz;
+tar xzvf gcc-${GCC_VERSION}.tar.gz;
+mkdir obj.gcc-${GCC_VERSION};
+cd gcc-${GCC_VERSION};
+./contrib/download_prerequisites;
+cd ../obj.gcc-${GCC_VERSION};
+../gcc-${GCC_VERSION}/configure --disable-multilib --enable-languages=c,c++ --disable-lto;
+make -j $(nproc);
+make install ;
+echo 'GCC-${GCC_VERSION} has been installed, below is the version information';
+gcc -v
+
+
