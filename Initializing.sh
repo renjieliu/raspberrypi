33a8518dea411011e65343b39dbb14391dffda12
renjieliu <anlrj@qq.com>
2020-08-17 10:47:02 -0400

    Update Initializing.sh

--- Initializing.sh
+++ Initializing.sh
@@ -1,7 +1,7 @@
 passwd;
 sudo passwd;
 sudo apt-get update; 
-sudo apt-get install -y locate mariadb-server tightvncserver;
+sudo apt-get install -y locate mariadb-server tightvncserver xrdp;
 sudo updatedb;
 sudo echo 'interface wlan0
 static ip_address=192.168.1.205/24
