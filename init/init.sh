c886d4202570cecfcea129224ff60c5a29148289
Renjie Liu <anlrj@qq.com>
2025-10-14 10:12:59 -0400

    init folder revamp

--- init/init.sh
+++ init/init.sh
@@ -8,14 +8,14 @@ sudo ./apt_install.sh
 
 sudo ./download_and_install_apps.sh
 
-sudo -u pi ./config.sh
+sudo -u pi ./pi_config.sh
 
 sudo ./fix_cleanup.sh
 
+echo "Initialization complete.."
 
+echo "NOTE: alias the sms_when_job_done from my tech journaling doc"
 
-echo "alias the sms_when_job_done from my tech journaling doc"
-echo "Initialization is complete..."
 
 
 
