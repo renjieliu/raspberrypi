83ca765080bbbe87211801d22c2ab8e15f31682f
Renjie Liu <anlrj@qq.com>
2025-10-20 16:46:48 -0400

    raspberrypi/rpi_init/apt_install.sh

--- rpi_init/apt_install.sh
+++ rpi_init/apt_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 sudo apt update 
-sudo apt-get install -y locate nano wget curl git net-tools iputils-ping gnupg2 pass xrdp zstd
+sudo apt-get install -y locate nano wget curl git net-tools iputils-ping gnupg2 pass xrdp zstd msmtp msmtp-mta mailutils
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker $USER
 
