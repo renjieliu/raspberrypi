4dd9c2d1a3d28469861f31b660b2dc0686f55af6
Renjie Liu <anlrj@qq.com>
2025-10-20 14:41:11 -0400

    adding a starting Notes.txt

--- rpi_init/apt_install.sh
+++ rpi_init/apt_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
 sudo apt update 
-
 sudo apt-get install -y locate nano wget curl git net-tools iputils-ping gnupg2 pass xrdp zstd
-
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker $USER
+
+
