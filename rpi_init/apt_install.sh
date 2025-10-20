b525d87e5e2935b162c5ff974942b32a84e69bae
Renjie Liu <anlrj@qq.com>
2025-10-20 10:16:08 -0400

    .

--- rpi_init/apt_install.sh
+++ rpi_init/apt_install.sh
@@ -4,6 +4,5 @@ sudo apt update
 
 sudo apt-get install -y locate nano wget curl git net-tools iputils-ping gnupg2 pass xrdp zstd
 
-
-
-
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker $USER
--- rpi_init/download_and_install_apps.sh
+++ rpi_init/download_and_install_apps.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 
+# RL 20251020 - Unable to come up with a generic solution to download the latest release from github, as developer uses differnt naming convention for the release 
+# some are using *-aarch.tar.gz, some are using zip. Since this file will be run only once, coding once for each app, it's better to check the extension manually when adding to this script.
+
+
 set -e
 
 arch=$(uname -m)
-
-
-##################################################
-########## Microsoft Edit ##########
-##################################################
-
-mkdir /tmp/msedit_download_tmp
-cd /tmp/msedit_download_tmp
-wget $(curl -s https://api.github.com/repos/microsoft/edit/releases/latest | grep "browser_download_url.*$arch.*zst" | awk 'BEGIN{FS=": "} {print $2}' | tr -d '"')
-tar -I zstd -xvf *.zst
-sudo cp ./edit /usr/local/bin/msedit
-sudo ln -s /usr/local/bin/msedit /usr/local/bin/me
-cd ~
-rm -rf /tmp/msedit_download_tmp
 
 
 
@@ -35,6 +25,47 @@ sudo ln -s /usr/local/bin/yt-dlp/yt-dlp_linux_$arch /usr/local/bin/yt
 cd ~
 rm -rf /tmp/yt_downloader_tmp
 
+
+
+
+##################################################
+########## Microsoft Edit ##########
+##################################################
+
+git_repo="https://github.com/microsoft/edit" # change here for the repo 
+git_release=$(echo $git_repo | sed 's|https://github.com/||g') # from the git_repo, get the username_repository
+tmp_dir_name=$(echo "$git_release"_tmp | sed 's|/|_|g' )  # tmp folder name, like microsoft_edit_tmp
+
+mkdir "/tmp/$tmp_dir_name"
+cd "/tmp/$tmp_dir_name"
+wget $(curl -s https://api.github.com/repos/$git_release/releases/latest | grep "browser_download_url.*$arch.*zst" | awk 'BEGIN{FS=": "} {print $2}' | tr -d '"')
+tar -I zstd -xvf *.zst
+sudo cp ./edit /usr/local/bin/msedit
+sudo ln -sf /usr/local/bin/msedit /usr/local/bin/me
+cd ~
+rm -rf "/tmp/$tmp_dir_name"
+
+
+
+##################################################
+########## bat ##########
+##################################################
+
+
+git_repo="https://github.com/sharkdp/bat" # change here for the repo 
+git_release=$(echo $git_repo | sed 's|https://github.com/||g') # from the git_repo, get the username_repository
+tmp_dir_name=$(echo "$git_release"_tmp | sed 's|/|_|g' )  # tmp folder name, like microsoft_edit_tmp
+
+mkdir "/tmp/$tmp_dir_name"
+cd "/tmp/$tmp_dir_name"
+wget $(curl -s https://api.github.com/repos/$git_release/releases/latest | grep "browser_download_url.*$arch.*unknown-linux-gnu.tar.gz" | awk 'BEGIN{FS=": "} {print $2}' | tr -d '"')
+tar -zxvf *.tar.gz
+rm *.tar.gz
+sudo mkdir /usr/local/bin/bat_folder
+sudo cp -rf ./bat* /usr/local/bin/bat_folder
+sudo ln -sf /usr/local/bin/bat_folder/bat /usr/local/bin/bat
+cd ~
+rm -rf "/tmp/$tmp_dir_name"
 
 
 
